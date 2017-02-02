c199d5249860cb87d29a25fe0a61abab05d4ea18
Krzysztof Molon <krzysztof.molon@intel.com>
2017-02-02 15:44:15 +0100

    A more elegant, but still not most elegant solution to merge sort in C++

--- mergeSort/C++/main.cpp
+++ mergeSort/C++/main.cpp
@@ -14,20 +14,19 @@ using std::string;
 // Contains already sorted entries
 #define PATH_SORTED "../sorted.txt"
 
-vector<int>* read_table(const char *path) {
-    // Reads the contents of an unsorted table and returns pointer to
-    // User must provide amount argument, where the amount of numbers to sort
-    // will be stored
+int read_table(const char *path, vector<int> &tab) {
+    // Reads the contents of an unsorted table and puts it into vector
     //
     // Args:
     //   path: Path to file to open
+    //   tab: vector to be filled with data
     //
     // Returns:
-    //   Pointer to vector containing integers
+    //   0 if successful, -1 otherwise
     ifstream f(path);
     if (!f.is_open()) {
         cout << "Failed to open file: " << path << endl;
-        return 0;
+        return -1;
     }
 
     // Omit the first two lines, there are just comments
@@ -38,19 +37,17 @@ vector<int>* read_table(const char *path) {
     // Store the amount
     int amount;
     f >> amount;
+    int c = 0;
 
-    // Create the vector to hold the data
-    vector<int> *tab = new vector<int>();
-    int i;
-    while (f >> i) {
-        tab->push_back(i);
-    }
+    // Resize the vector
+    tab.resize(amount);
+    while(f >> tab[c++]) ;
 
     f.close();
-    return tab;
+    return 0;
 }
 
-static void _merge(vector<int> *tab, int start, int end, vector<int> *buffer) {
+static void _merge(vector<int> &tab, int start, int end, vector<int> &buffer) {
     // Merges table
     //
     // Args:
@@ -65,28 +62,28 @@ static void _merge(vector<int> *tab, int start, int end, vector<int> *buffer) {
 
     // Copy data into buffer, it will be sorted data
     while ((left < half) && (right < end)) {
-        if ((*tab)[left] < (*tab)[right]) {
-            (*buffer)[bufPtr++] = (*tab)[left++];
+        if (tab[left] < tab[right]) {
+            buffer[bufPtr++] = tab[left++];
         } else {
-            (*buffer)[bufPtr++] = (*tab)[right++];
+            buffer[bufPtr++] = tab[right++];
         }
     }
 
     while (left < half) {
-        (*buffer)[bufPtr++] = (*tab)[left++];
+        buffer[bufPtr++] = tab[left++];
     }
 
     while (right < end) {
-        (*buffer)[bufPtr++] = (*tab)[right++];
+        buffer[bufPtr++] = tab[right++];
     }
 
     // Now copy the sorted data to original table
     for (left = start; left < end; ++left) {
-        (*tab)[left] = (*buffer)[left];
+        tab[left] = buffer[left];
     }
 }
 
-static void _merge_sort(vector<int> *tab, int start, int end, vector<int> *buffer) {
+static void _merge_sort(vector<int> &tab, int start, int end, vector<int> &buffer) {
     // Sort the provided table from start to end. Allocated buffer must be
     // provided to store the sorted data
     //
@@ -108,18 +105,16 @@ static void _merge_sort(vector<int> *tab, int start, int end, vector<int> *buffe
     _merge(tab, start, end, buffer);
 }
 
-void merge_sort(vector<int> *tab) {
+void merge_sort(vector<int> &tab) {
     // Calls the underlying mergeSort algorithm and sorts the provided vector
     //
     // Args:
     //   tab: vector to sort
-    vector<int> *buffer = new vector<int>(tab->size());
-    _merge_sort(tab, 0, tab->size(), buffer);
-
-    delete buffer;
+    vector<int> buffer(tab.size());
+    _merge_sort(tab, 0, tab.size(), buffer);
 }
 
-int compare_table(vector<int> *tab1, vector<int> *tab2) {
+int compare_table(vector<int> &tab1, vector<int> &tab2) {
     // Compares two tables, it is quietly assumed, that both have the same
     // amount of entries. If there are is a single diffirence, it is pointed out
     //
@@ -130,10 +125,10 @@ int compare_table(vector<int> *tab1, vector<int> *tab2) {
     // Returns:
     //   0 if vectors are the same
     int i;
-    for (i = 0; i < tab1->size(); ++i) {
-        if ((*tab1)[i] != (*tab2)[i]) {
+    for (i = 0; i < tab1.size(); ++i) {
+        if (tab1[i] != tab2[i]) {
             cout << "Diffirence at: " << i << endl;
-            cout << ">>>" << (*tab1)[i] << " " << (*tab2)[i] << endl;
+            cout << ">>>" << tab1[i] << " " << tab2[i] << endl;
             return -1;
         }
     }
@@ -142,17 +137,16 @@ int compare_table(vector<int> *tab1, vector<int> *tab2) {
 }
 
 int main(int argc, char *argv[]) {
-    vector<int> *to_sort = read_table(PATH_TO_SORT);
+    vector<int> to_sort;
 
-    if (to_sort == 0) {
+    if (read_table(PATH_TO_SORT, to_sort) != 0) {
         cout << "Failed to read table from file: " << PATH_TO_SORT << endl;
         return -1;
     }
-    vector<int> *sorted = read_table(PATH_SORTED);
+    vector<int> sorted;
 
-    if (sorted == 0) {
+    if (read_table(PATH_SORTED, sorted) != 0) {
         cout << "Failed to read table from file: " << PATH_SORTED << endl;
-        delete to_sort;
         return -1;
     }
 
@@ -160,13 +154,9 @@ int main(int argc, char *argv[]) {
 
     if (compare_table(to_sort, sorted) != 0) {
         cout << "Table wasn't sorted correctly!" << endl;
-        delete to_sort;
-        delete sorted;
         return -1;
     }
 
-    delete to_sort;
-    delete sorted;
     cout << "Table sorted correctly!" << endl;
     return 0;
 }
