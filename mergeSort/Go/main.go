cb91a5c245de1d81afdcab5cc5675a8c65b3994e
Melon <wielkizielonymelon@gmail.com>
2017-02-09 14:30:58 +0100

    Some dummy file reading in Go

--- mergeSort/Go/main.go
+++ mergeSort/Go/main.go
@@ -1,8 +1,55 @@
 package main
 
-import ("fmt")
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func main() {
-    fmt.Println("Table wasn't sorted correctly")
+const file_to_sort = "../toSort.txt"
+const file_sorted = "../sorted.txt"
+
+func readNumber(path string) []int {
+	file, err := os.Open(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// Omit first two lines
+	scanner.Scan()
+	scanner.Scan()
+
+	// Store the amount
+	scanner.Scan()
+	amount, _ := strconv.Atoi(scanner.Text())
+
+	// Fill the array with data
+	toSort := make([]int, amount)
+	i := 0
+	for scanner.Scan() {
+		toSort[i], _ = strconv.Atoi(scanner.Text())
+		i = i + 1
+	}
+
+	return toSort
 }
 
+func main() {
+	toSort := readNumber(file_to_sort)
+	if toSort[0] > 1 {
+		fmt.Println("done")
+	}
+
+	sorted := readNumber(file_sorted)
+	if sorted[0] != 1 {
+		fmt.Println("done")
+	}
+
+	fmt.Println("Table wasn't sorted correctly")
+}
