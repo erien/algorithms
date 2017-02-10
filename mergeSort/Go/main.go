971e18003263095a7462a0a7ac54f5835734f9ca
Melon <wielkizielonymelon@gmail.com>
2017-02-10 10:34:58 +0100

    Draft of the merge sort algorithm for Go

--- mergeSort/Go/main.go
+++ mergeSort/Go/main.go
@@ -33,7 +33,7 @@ func cmpArrays(a []int, b []int) bool {
 	return true
 }
 
-func readNumber(path string) []int {
+func readNumbers(path string) []int {
 	file, err := os.Open(path)
 
 	if err != nil {
@@ -62,9 +62,63 @@ func readNumber(path string) []int {
 	return toSort
 }
 
+func _merge(toSort []int, start int, half int, end int, buffer []int) {
+	left := start
+	right := half
+	i := start
+
+	for left < half && right < end {
+		if toSort[left] < toSort[right] {
+			buffer[i] = toSort[left]
+			left += 1
+		} else {
+			buffer[i] = toSort[right]
+			right += 1
+		}
+		i += 1
+	}
+
+	for left < half {
+		buffer[i] = toSort[left]
+		i += 1
+		left += 1
+	}
+
+	for right < end {
+		buffer[i] = toSort[right]
+		i += 1
+		right += 1
+	}
+
+	i = start
+	for i < end {
+		toSort[i] = buffer[i]
+		i += 1
+	}
+
+}
+
+func _mergeSort(toSort []int, start int, end int, buffer []int) {
+	if end-start <= 1 {
+		return
+	}
+
+	half := (end + start) / 2
+	_mergeSort(toSort, start, half, buffer)
+	_mergeSort(toSort, half, end, buffer)
+	_merge(toSort, start, half, end, buffer)
+}
+
+func mergeSort(toSort []int) {
+	buffer := make([]int, len(toSort))
+	_mergeSort(toSort, 0, len(toSort), buffer)
+}
+
 func main() {
-	toSort := readNumber(file_to_sort)
-	sorted := readNumber(file_sorted)
+	toSort := readNumbers(file_to_sort)
+	sorted := readNumbers(file_sorted)
+
+	mergeSort(toSort)
 
 	if cmpArrays(toSort, sorted) == false {
 		fmt.Println("Table wasn't sorted correctly")
